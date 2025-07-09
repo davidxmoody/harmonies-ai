24df84a36d1969e8e269709d6f134d44623aead3
David Moody <david@davidxmoody.com>
2025-07-09 13:34:29 +0100

    Make greedy algorithm consider all possible states

--- harmonies_ai/ai/greedy.py
+++ harmonies_ai/ai/greedy.py
@@ -1,16 +1,65 @@
-from random import choice
 from harmonies_ai.game_state import GameState
+from harmonies_ai.tokens import Token
+from itertools import product
+
+
+# TODO maybe move into the enum itself
+_placement_priority = [
+    Token.GRY,
+    Token.BRN,
+    Token.RED,
+    Token.GRN,
+    Token.BLU,
+    Token.YLW,
+]
+
+
+def sort_by_placement_priority(tokens: tuple[Token, ...]):
+    return tuple(sorted(tokens, key=lambda t: _placement_priority.index(t)))
+
+
+def has_conflict(gs: GameState, turn: tuple[tuple[Token, int]]):
+    updated_stacks = {}
+    for token, pos in turn:
+        stack = updated_stacks.get(pos, gs.board[pos])
+        if token not in stack.placements:
+            return True
+        updated_stacks[pos] = stack.placements[token]
+
+
+def get_next_states(gs: GameState):
+    token_spots = {
+        token: tuple(
+            pos
+            for pos, stack in enumerate(gs.board)
+            if token in stack.placements and not gs.cubes[pos]
+        )
+        for token in Token
+    }
+
+    turns = set()
+
+    for tokens in set(gs.display_tokens):
+        sorted_tokens = sort_by_placement_priority(tokens)
+        turns.update(
+            product(
+                *[
+                    [(token, pos) for pos in token_spots[token]]
+                    for token in sorted_tokens
+                ]
+            )
+        )
+
+    for turn in turns:
+        if not has_conflict(gs, turn):
+            yield gs.copy().place_token(*turn[0]).place_token(*turn[1]).place_token(
+                *turn[2]
+            )
 
 
 def simulate_game(gs: GameState):
     while not gs.game_ended:
-        tokens = choice(gs.display_tokens)
-        for token in tokens:
-            next_states = [
-                gs.copy().place_token(token, pos)
-                for pos, stack in enumerate(gs.board)
-                if token in stack.placements
-            ]
-            gs = max(next_states, key=lambda s: s.score.total)
+        next_states = get_next_states(gs)
+        gs = max(next_states, key=lambda s: s.score.total)
         gs.end_turn()
     return gs
--- harmonies_ai/ai/runner.py
+++ harmonies_ai/ai/runner.py
@@ -1,16 +1,19 @@
 from harmonies_ai.game_state import GameState
 from rich.console import Console
 from harmonies_ai.ai.greedy import simulate_game
+import pandas as pd
+from tqdm import tqdm
 
 console = Console()
 
 
-def main():
-    results = []
-    for _ in range(100):
-        results.append(simulate_game(GameState()))
-    return results
+# %%
+def results_to_df(results: list[GameState]):
+    return pd.DataFrame({"score": gs.score.total} for gs in results)
 
 
-if __name__ == "__main__":
-    main()
+# %%
+results = []
+for _ in tqdm(range(100), desc="Playing games"):
+    results.append(simulate_game(GameState()))
+df = results_to_df(results)
